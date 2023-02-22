fdcf134ed07ad22689fea89df0ed696bab541da6
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2023-02-21 21:46:58 -0500

    Initial commit
    Initial setup for the Photosnap Frontend Mentor challenge.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="min-h-screen grid place-items-center">
+      <h1 className="font-bold">PHOTOSNAP</h1>
+    </main>
+  );
+};
+
+export default App;
