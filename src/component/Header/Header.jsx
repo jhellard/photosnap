e6bd8bbd7d5d263a2891241efae2342ba311528d
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2023-03-08 14:55:40 -0500

    Improved Header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,23 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
 import Logo from "../../assets/shared/desktop/logo.svg";
 import Menu from "./assets/menu.svg";
 import Close from "./assets/close.svg";
 
 const Header = () => {
-  const menuToggle = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
     const isSmallScreen = window.innerWidth < 640;
-
-    if (isSmallScreen) {
-      const nav = document.querySelector("#nav");
-      const hamburger = document.querySelector("#nav-hamburger");
-      const overlay = document.querySelector("#overlay");
-
-      overlay.classList.toggle("hidden");
-      nav.classList.toggle("hidden");
-      nav.classList.toggle("flex");
-      hamburger.src = nav.classList.contains("hidden") ? Menu : Close;
-    }
+    isSmallScreen &&
+      document.querySelector("#overlay").classList.toggle("hidden");
   };
 
   return (
@@ -26,33 +20,22 @@ const Header = () => {
         <img src={Logo} alt="Photosnap Logo" />
       </Link>
       <nav
-        className="absolute top-[72px] left-0 hidden min-h-[253px] min-w-full flex-col justify-center bg-pureWhite px-6 text-[15px] font-bold tracking-[2.5px] sm:relative sm:top-0 sm:flex sm:min-h-[auto] sm:min-w-[auto] sm:px-4 sm:text-xs"
+        className={`absolute top-[72px] left-0 ${
+          menuOpen ? "flex" : "hidden"
+        } min-h-[253px] min-w-full flex-col justify-center bg-pureWhite px-6 text-[15px] font-bold tracking-[2.5px] sm:relative sm:top-0 sm:flex sm:min-h-[auto] sm:min-w-[auto] sm:px-4 sm:text-xs`}
         id="nav"
       >
         <ul className="mb-5 flex flex-col items-center gap-5 sm:mb-0 sm:flex-row sm:gap-[37px] sm:tracking-[2px]">
-          <Link
-            to={"/stories"}
-            className="hover:opacity-50"
-            onClick={() => menuToggle()}
-          >
-            STORIES
-          </Link>
-          <Link
-            to={"/features"}
-            className="hover:opacity-50"
-            onClick={() => menuToggle()}
-          >
-            FEATURES
-          </Link>
-          <Link
-            to={"/pricing"}
-            className="hover:opacity-50"
-            onClick={() => menuToggle()}
-          >
-            PRICING
-          </Link>
+          {["STORIES", "FEATURES", "PRICING"].map((link) => (
+            <Link
+              to={`/${link.toLowerCase()}`}
+              className="hover:opacity-50"
+              onClick={toggleMenu}
+            >
+              {link}
+            </Link>
+          ))}
         </ul>
-
         <hr className="mb-5 border-pureBlack opacity-25 sm:hidden" />
         <button className="min-h-[3rem] min-w-full bg-pureBlack font-bold tracking-[inherit] text-pureWhite sm:hidden">
           GET AN INVITE
@@ -61,12 +44,12 @@ const Header = () => {
       <button className="hidden min-h-[40px] min-w-[158px] bg-pureBlack text-xs font-bold tracking-[2.5px] text-pureWhite hover:bg-lightGrey hover:text-pureBlack sm:block">
         GET AN INVITE
       </button>
-      <button
-        id="nav-toggle"
-        className="sm:hidden"
-        onClick={() => menuToggle()}
-      >
-        <img src={Menu} id="nav-hamburger" alt="Menu Toggle" />
+      <button id="nav-toggle" className="sm:hidden" onClick={toggleMenu}>
+        <img
+          src={menuOpen ? Close : Menu}
+          id="nav-hamburger"
+          alt="Menu Toggle"
+        />
       </button>
     </header>
   );
